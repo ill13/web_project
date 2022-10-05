c24711e4c1d0c32e3037f30bea41c2dc0ef45ee5
ill13 <ill13@ill13.com>
2022-10-04 21:57:12 -0400

    splitting off to make a side project

--- 01_HelloWorld_Jinja2/app/main.py
+++ 01_HelloWorld_Jinja2/app/main.py
@@ -5,12 +5,12 @@ from fastapi.staticfiles import StaticFiles
 
 from datetime import datetime
 
-
+# Create an f string
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 my_string=f"Hello World using FastAPI, uvicorn, and Jinja2! It's {dt_string}!"
 
-# Open a local text file and add each line as a list item
+# Open a local text file and add each line to a list as an item
 with open('text.txt') as f:
     my_list = list(f)
 
@@ -24,3 +24,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static") # folder co
 
 async def index(request: Request):
     return templates.TemplateResponse("index.html",context={'request':request,'title':"Demo0", 'content':my_string, 'my_list':my_list})
+
+
+# @app.get("/butt")
+
+# async def index(request: Request):
+#     return templates.TemplateResponse("index.html",context={'request':request,'title':"Demo0", 'content':my_string, 'my_list':my_list})
